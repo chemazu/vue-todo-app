575cb21cd666dcf531d87312e6d240123b9c1edf
chemazu <chukwuemekachemazu@gmail.com>
2022-08-07 16:42:02 +0100

    created components, can use import ,exports

---
+++ src/components/About/About.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="about">
+    <h1>About</h1>
+    <p>This is the about page</p>
+  </div>
+</template>
+
+<script lang="ts">
+import { Options, Vue } from "vue-class-component";
+import "./about.css";
+
+@Options({
+  props: {
+    title: String,
+  },
+})
+export default class About extends Vue {
+  title!: string;
+}
+</script>
+;
---
+++ src/components/Home.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="home">
+    <h1>{{ title }}</h1>
+  </div>
+</template>
+
+<script lang="ts">
+import { Options, Vue } from "vue-class-component";
+import "./home.css";
+
+@Options({
+  props: {
+    title: String,
+  },
+})
+export default class Home extends Vue {
+  title!: string;
+}
+</script>
+;
